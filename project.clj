58c8bbbdc6d2e2318714f5ac02c331d52ff0af19
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-03-22 16:04:39 +1100

    Added a project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject tpds-2012-simulation "1.0.0-SNAPSHOT"
-  :description ""
+  :description "Simulations for the paper entitled "Host Overload Detection for Dynamic Consolidation of Virtual Machines in Clouds Based on a Markov Chain Model and Multisize Sliding Window Workload Estimation" submitted to the Special Issue of TPDS on Cloud Computing."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]
                  [org.clojure/data.json "0.1.2"]
